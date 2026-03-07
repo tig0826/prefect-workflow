7d035f40b249064be69e7dff734bc5635a1cbcd9
tig <tigcr0826@gmail.com>
2026-03-07 23:46:34 +0900

    fix trino port

--- dqx/sync_supabase_mrt_price_hourly/sync_supabase_mrt_price_hourly.py
+++ dqx/sync_supabase_mrt_price_hourly/sync_supabase_mrt_price_hourly.py
@@ -1,4 +1,3 @@
-import pandas as pd
 from prefect import flow, task
 from prefect import get_run_logger
 
@@ -7,11 +6,10 @@ from prefect.blocks.system import Secret
 
 from common.trino_api import TrinoAPI
 
-trino_host = 'trino.trino.svc.cluster.local'
+trino_host = "trino.trino.svc.cluster.local"
 trino_port = 8080
-trino_user = 'tig'
-trino_catalog = 'iceberg'
-
+trino_user = "tig"
+trino_catalog = "iceberg"
 
 
 class SupabaseDB:
@@ -20,6 +18,7 @@ class SupabaseDB:
         key = Secret.load("supabase-key").get()
         self.client: Client = create_client(url, key)
         self.database_name = "dqx_trade_db"
+
     def add_record(self, record):
         """取引記録を追加"""
         try:
@@ -29,13 +28,13 @@ class SupabaseDB:
             print(f"レコード追加失敗: {e}")
             return False
 
+
 @task(retries=3, retry_delay_seconds=10)
 def get_price_data():
     logger = get_run_logger()
-    trino = TrinoAPI(host=trino_host,
-                     port=trino_port,
-                     user=trino_user,
-                     catalog=trino_catalog)
+    trino = TrinoAPI(
+        host=trino_host, port=trino_port, user=trino_user, catalog=trino_catalog
+    )
     query = """
             SELECT *
             FROM dqx.mrt_price_hourly
@@ -46,15 +45,14 @@ def get_price_data():
             """
     df = trino.execute_query(query)
     logger.info(f"Fetched {len(df)} records from Trino")
-    focus_columns = ['item_id',
-                     'ts_hour',
-                     'p5_price']
+    focus_columns = ["item_id", "ts_hour", "p5_price"]
     df = df[focus_columns]
     # 価格を小数点第一位で丸める
     df.loc[:, "p5_price"] = df["p5_price"].round(1)
     # supabaseに登録するためにデータ型を変換
     df["ts_hour"] = df["ts_hour"].astype(str)
     return df
+
 
 @task(retries=3, retry_delay_seconds=10)
 def add_price_record(record):
@@ -71,5 +69,5 @@ def add_price_record(record):
 def sync_supabase_mrt_price_hourly():
     logger = get_run_logger()
     df = get_price_data()
-    record = df.to_dict(orient='records')
+    record = df.to_dict(orient="records")
     add_price_record(record)
